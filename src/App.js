60070142d153c936274cdcb2d5897a2bcb0fbcaf
a-wotton <a_wotton@fanshaweonline.ca>
2024-03-07 17:05:18 -0500

    setup the project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>To Do List:</h1>
+        <form>
+          <input type="text"/>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
